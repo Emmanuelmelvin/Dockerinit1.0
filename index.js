1d31d6ef54285c80aef5115986d710856d7318b5
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-06-30 13:34:36 -0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/' , (req , res) => {
-    res.send('<h2> Hi There  , my name is Akin.</h2>')
+    res.send('<h2> Hi There  , my name is Emmanuel.</h2>')
 })
 
 app.listen( port , ()=> console.log(`listening on port${port}`))
