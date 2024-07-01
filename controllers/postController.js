49c4966de9a5050bb73d8ff1d5d7397436b9015b
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-07-01 13:07:39 -0700

    Added folders

---
+++ controllers/postController.js
@@ -0,0 +1,92 @@
+const Post = require('../models/postModels')
+
+exports.getAllPosts = async (req, res, next) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json({
+            status: "success",
+            data: {
+                length: posts.length,
+                posts
+            }
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed'
+        })
+        console.log(error)
+    }
+
+}
+
+exports.getOnePost = async (req, res) => {
+    try {
+
+        const { id } = req.params.id
+        const post = await Post.findById(id)
+        res.status(201).json({
+            status: "success",
+            data: post
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed'
+        })
+        console.log(error)
+    }
+}
+
+
+exports.createPost = async (req, res) => {
+    try {
+        const post = Post.createOne(req.body)
+        res.status(201).json({
+            status: "success",
+            data: post
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed'
+        })
+        console.log(error)
+    }
+}
+
+exports.updatePost = async (req, res) => {
+    try {
+
+        const { id } = req.params.id
+        const post = await Post.findByIdAndUpdate(id , req.body)
+        res.status(201).json({
+            status: "success",
+            data: post
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed'
+        })
+        console.log(error)
+    }
+}
+
+exports.getOnePost = async (req, res) => {
+    try {
+
+        const { id } = req.params.id
+        const post = await Post.findById(id)
+        res.status(201).json({
+            status: "success",
+            data: post
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed'
+        })
+        console.log(error)
+    }
+}
+
