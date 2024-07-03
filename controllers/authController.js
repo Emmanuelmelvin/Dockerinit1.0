d20b921b69ad3161c7e9679a6b82153bc6d23e70
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2024-07-03 08:28:06 -0700

    Added routes for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 const User = require('../models/userModel')
 const bcrypt = require("bcryptjs")
 
-exports.signUp = async (req , res) => {
-    try{
-        const { username , password} = req.body
-        const hashPassword = await bcrypt.hash(password , 12)
+exports.signUp = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const hashPassword = await bcrypt.hash(password, 12)
 
         const newUser = await User.create(
             {
@@ -18,7 +18,38 @@ exports.signUp = async (req , res) => {
                 user: newUser
             }
         })
-    }catch (error) {
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({
+            status: "failed"
+        })
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const user = await User.findOne({ username })
+
+        if (!user) {
+            return res.status(404).json({
+                status: "fail",
+                message: "User not found"
+            })
+        }
+        const isCorrect = await bcrypt.compare(password, user.password)
+
+        if(isCorrect){
+            return res.status(200).json({
+                status: "success",
+            })
+        } else{
+            return res.status(400).json({
+                status: "failed"
+            })
+        }
+        
+    } catch (error) {
         console.log(error)
         res.status(401).json({
             status: "failed"
